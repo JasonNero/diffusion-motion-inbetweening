c43faf1987229dfbc26fddf998256c8133e1eee0
Jason Schuehlein <jason@schuehlein.eu>
2024-07-09 11:12:10 +0200

    add prepare/download_pretrained.sh

---
+++ prepare/download_pretrained.sh
@@ -0,0 +1,22 @@
+mkdir -p save/
+cd save/
+
+echo "The pretrained models will be stored in the 'save' folder\n"
+
+# condmdi_random_frames
+echo "Downloading the condmdi_random_frames model"
+gdown "15mYPp2U0VamWfu1SnwCukUUHczY9RPIP"
+echo "Extracting the condmdi_random_frames model"
+unzip condmdi_random_frames.zip
+echo "Cleaning\n"
+rm condmdi_random_frames.zip
+
+# condmdi_uncond
+echo "Downloading the condmdi_uncond model"
+gdown "1B0PYpmCXXwV0a5mhkgea_J2pOwhYy-k5"
+echo "Extracting the condmdi_uncond model"
+unzip condmdi_uncond.zip
+echo "Cleaning\n"
+rm condmdi_uncond.zip
+
+echo "Downloading done!"
