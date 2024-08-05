60a507e01ce1ab4aafd33399de71c233f41e8468
Jason Schuehlein <jason@schuehlein.eu>
2024-08-05 11:15:41 +0200

    add condmdi_random_joints to downloader

--- prepare/download_pretrained.sh
+++ prepare/download_pretrained.sh
@@ -3,6 +3,14 @@ cd save/
 
 echo "The pretrained models will be stored in the 'save' folder\n"
 
+# condmdi (aka condmdi_random_joints)
+echo "Downloading the condmdi model"
+gdown "1aP-z1JxSCTcUHhMqqdL2wbwQJUZWHT2j"
+echo "Extracting the condmdi model"
+unzip condmdi_random_joints.zip
+echo "Cleaning\n"
+rm condmdi_random_joints.zip
+
 # condmdi_random_frames
 echo "Downloading the condmdi_random_frames model"
 gdown "15mYPp2U0VamWfu1SnwCukUUHczY9RPIP"
